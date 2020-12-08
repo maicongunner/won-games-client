3cf70b50e9c3166517c0e4c0be42bd2aaf64aadf
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-08 08:23:17 -0300

    impr: create a new helper to unit tests using theme project

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,11 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render white color by default', () => {
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
   })
 })
