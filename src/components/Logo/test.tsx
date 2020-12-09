680640a5a89641bbe45d0be2b5622e643b9eac34
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-09 07:49:50 -0300

    impr: adds some unit tests to validate the width of logo on render

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -19,4 +19,18 @@ describe('<Logo />', () => {
     });
   });
 
+  it('should render a big logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    });
+  });
+
+  it('should render a normal logo', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    });
+  });
+
 });
