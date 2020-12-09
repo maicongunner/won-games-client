ad17b5966d3ece586e01ac4ac204891f76fbafe6
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-09 07:34:49 -0300

    impr: adds some unit tests to render logo element

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render white color by default', () => {
 
-  })
-})
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    });
+  });
+
+  it('should render a black color when is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    });
+  });
+
+});
