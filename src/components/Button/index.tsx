4d9aa9375ba5a5b58103f6e6f335d8a71fda3f6f
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-15 07:19:51 -0300

    impr: implements a fullWidth version button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,12 @@ import * as S from './styles'
 
 export type ButtonProps = {
   children?: React.ReactNode,
-  size?: 'small' | 'medium' | 'large'
+  size?: 'small' | 'medium' | 'large',
+  fullWidth?: boolean
 }
 
-const Button = ({ children, size = 'medium' }: ButtonProps) => (
-  <S.Wrapper size={size}>{!!children && <span>{children}</span>}</S.Wrapper>
+const Button = ({ children, size = 'medium', fullWidth }: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth}>{!!children && <span>{children}</span>}</S.Wrapper>
 )
 
 export default Button
