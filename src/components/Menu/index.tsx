e78dd5955d36a0daeedbe3791f143a61c4351198
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-17 08:02:02 -0300

    impr: adding the logic to the desktop menu and adds styles them

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import Logo from 'components/Logo'
 import * as S from './styles'
 import Button from 'components/Button'
+import MediaMatch from 'components/MediaMatch'
 
 export type MenuProps = {
   username?: string
@@ -18,18 +19,36 @@ const Menu = ({ username }: MenuProps) => {
   return (
     <S.Wrapper>
       <S.IconWrapper>
-        <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
+        <MediaMatch lessThan="medium">
+          <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
+        </MediaMatch>
       </S.IconWrapper>
+
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
         </S.IconWrapper>
+
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign In</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
