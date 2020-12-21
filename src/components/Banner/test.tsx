6185db041fe61ea3ac27769ce160ad9995933c7f
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-21 08:04:30 -0300

    feat: creates a new Banner component and adding styles and unit tests for it

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subTitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new crashLands season/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
