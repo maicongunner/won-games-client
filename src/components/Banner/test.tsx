dc6252740a70baf06449947a6948826fbbac0fed
Maicon Domingues <maikonbdomingues@gmail.com>
2020-12-22 08:11:52 -0300

    feat: adding new type of Banner with Ribbon and unit tests

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -24,4 +24,26 @@ describe('<Banner />', () => {
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
 })
