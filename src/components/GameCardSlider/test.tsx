0e872ebbdbd8066b329e9e5f5658cf6442ff5dd5
Maicon Domingues <maikonbdomingues@gmail.com>
2021-01-21 19:55:31 -0300

    feat: create a new GameCardSlider component with unit tests

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -1,15 +1,78 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCardSlider from '.'
 
 describe('<GameCardSlider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCardSlider />)
+  const items = [
+    {
+      title: 'Population Zero',
+      developer: 'Rockstar Games',
+      img: 'https://source.unsplash.com/user/willianjusten/300x140',
+      price: 'R$ 235,00',
+      promotionalPrice: 'R$ 215,00'
+    },
+    {
+      title: 'Population Zero',
+      developer: 'Rockstar Games',
+      img: 'https://source.unsplash.com/user/willianjusten/300x141',
+      price: 'R$ 235,00',
+      promotionalPrice: 'R$ 215,00'
+    },
+    {
+      title: 'Population Zero',
+      developer: 'Rockstar Games',
+      img: 'https://source.unsplash.com/user/willianjusten/300x142',
+      price: 'R$ 235,00',
+      promotionalPrice: 'R$ 215,00'
+    },
+    {
+      title: 'Population Zero',
+      developer: 'Rockstar Games',
+      img: 'https://source.unsplash.com/user/willianjusten/300x143',
+      price: 'R$ 235,00',
+      promotionalPrice: 'R$ 215,00'
+    },
+    {
+      title: 'Population Zero',
+      developer: 'Rockstar Games',
+      img: 'https://source.unsplash.com/user/willianjusten/300x144',
+      price: 'R$ 235,00',
+      promotionalPrice: 'R$ 215,00'
+    },
+    {
+      title: 'Population Zero',
+      developer: 'Rockstar Games',
+      img: 'https://source.unsplash.com/user/willianjusten/300x145',
+      price: 'R$ 235,00',
+      promotionalPrice: 'R$ 215,00'
+    }
+  ]
 
-    expect(
-      screen.getByRole('heading', { name: /GameCardSlider/i })
-    ).toBeInTheDocument()
+  describe('<GameCardSlider />', () => {
+    it('should render correctly', () => {
+      const { container } = renderWithTheme(<GameCardSlider items={items} />)
 
-    expect(container.firstChild).toMatchSnapshot()
+      expect(container.querySelector('.slick-slide')).toBeInTheDocument()
+      expect(container.querySelectorAll('.slick-slide')).toHaveLength(6)
+    })
+
+    it('should render with 4 elements active only', () => {
+      const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+      expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+    })
+
+    it('should render white arrows if color passed', () => {
+      renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+      expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+        color: '#FAFAFA'
+      })
+      expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+        color: '#FAFAFA'
+      })
+    })
   })
 })
