0964deeeeaf0f5de2914964638ba72495d6a2317
Maicon Domingues <maikonbdomingues@gmail.com>
2021-01-22 08:21:52 -0300

    impr: sets custom background colors for the storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,22 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
+
 addDecorator(withNextRouter())
 
 export const decorators = [
