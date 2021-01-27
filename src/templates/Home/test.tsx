43fe9da649a895ac64f5b5b093f65bb784107cdc
Maicon Domingues <maikonbdomingues@gmail.com>
2021-01-27 07:59:04 -0300

    impr: improves the unit tests from home page

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -2,22 +2,40 @@ import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
 import Home from '.'
+
+const props = {
+  banners: bannerMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 
+    // logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    // footer
     expect(
       screen.getByRole('heading', { name: /contact us/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render the sections', () => {
-    renderWithTheme(<Home />)
-
+    // sections
     expect(
       screen.getByRole('heading', { name: /most popular/i })
     ).toBeInTheDocument()
@@ -31,5 +49,14 @@ describe('<Home />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+
+    // highlight
+    expect(screen.getAllByText(/read dead it's back/i)).toHaveLength(3)
   })
 })
