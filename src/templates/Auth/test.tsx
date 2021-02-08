1350beff199f28fb2e74af71b5b9a8e60f435b7b
Maicon Domingues <maikonbdomingues@gmail.com>
2021-02-08 07:42:57 -0300

    feat: create SignIn and SignUp pages and adds unit test to Auth Page

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,13 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /Won Games/i })).toHaveLength(2)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', {
+        name: /All yout favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best of most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Auth title/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByText(/Won Games 2021 Todos os direitos reservados/i)
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
