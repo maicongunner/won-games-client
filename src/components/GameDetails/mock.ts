9cdc29ee074d1622be85d6b4cb207eef7ff85b3f
Maicon Domingues <maikonbdomingues@gmail.com>
2021-02-15 15:41:13 -0300

    feat: creates a new GameDetails component with unit tests

---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  developer: 'Different Tales',
+  platforms: ['windows', 'mac', 'linux'],
+  releaseDate: '2020-11-21T23:00:00',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Narrative']
+}
