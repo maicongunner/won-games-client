2dfa38d2df0e28f9fd85a47c84cd96b0362a0eda
Maicon Domingues <maikonbdomingues@gmail.com>
2021-02-17 10:57:34 -0300

    feat: adds section GameInfo into Game Page Details

--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,3 +1,46 @@
-import styled from 'styled-components'
+import { Container } from 'components/Container'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Main = styled.main`
+  margin-top: 20rem;
+
+  ${media.greaterThan('medium')`
+    margin-top: 30rem;
+  `}
+`
+
+type CoverProps = {
+  src: string
+}
+
+export const Cover = styled.div<CoverProps>`
+  ${({ src }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 39.5rem;
+    background-image: url(${src});
+    background-size: cover;
+    background-position: top center;
+    opacity: 0.4;
+
+    ${media.greaterThan('medium')`
+      height: 50rem;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
+    `}
+  `}
+`
+
+const Section = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.xlarge};
+
+    ${media.greaterThan('medium')`
+      margin-bottom: calc(${theme.spacings.xlarge} * 2);
+    `}
+  `}
+`
+
+export const SectionGameInfo = styled(Section)``
