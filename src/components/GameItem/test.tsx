1f5afc8ba86de8457a265ccba05ae28a543c11c9
Maicon Domingues <maikonbdomingues@gmail.com>
2021-03-11 07:38:18 -0300

    feat: creates a new GameItem component with unit tests

---
+++ src/components/GameItem/test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+
+import GameItem from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00'
+}
+
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    render(<GameItem {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByText(props.price)).toBeInTheDocument()
+  })
+})
