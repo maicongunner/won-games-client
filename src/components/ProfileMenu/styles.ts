6fc6d6c4e111eed13cf232a46117e4bbc862efb3
Maicon Domingues <maikonbdomingues@gmail.com>
2021-03-18 07:47:14 -0300

    feat: adds a new feature link active from ProfileMenu component

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 
 export const Nav = styled.nav`
@@ -17,10 +17,23 @@ export const Nav = styled.nav`
   `}
 `
 
-export const Link = styled.a`
-  ${({ theme }) => css`
+const linkModifier = {
+  default: (theme: DefaultTheme) => css`
     background: ${theme.colors.white};
     color: ${theme.colors.black};
+  `,
+  active: (theme: DefaultTheme) => css`
+    background: ${theme.colors.primary};
+    color: ${theme.colors.white};
+  `
+}
+
+type linkProps = {
+  isActive?: boolean
+}
+
+export const Link = styled.a<linkProps>`
+  ${({ theme, isActive }) => css`
     text-decoration: none;
     display: flex;
     align-items: center;
@@ -44,5 +57,8 @@ export const Link = styled.a`
         display: none;
       }
     `}
+
+    ${!isActive && linkModifier.default(theme)}
+    ${isActive && linkModifier.active(theme)}
   `}
 `
