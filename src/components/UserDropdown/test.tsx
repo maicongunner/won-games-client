5ec6099bc3bbdf593fd322a4459840c0ddda0168
Maicon Domingues <maikonbdomingues@gmail.com>
2021-04-07 08:20:27 -0300

    feat: creates a UserDropdown component with unit tests

---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Maicon" />)
+
+    expect(screen.getByText('Maicon')).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Maicon" />)
+
+    userEvent.click(screen.getByText('Maicon'))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+})
