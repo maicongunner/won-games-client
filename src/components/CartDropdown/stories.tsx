dc16307af928521ef961e688b4eca715b1632f9d
Maicon Domingues <maikonbdomingues@gmail.com>
2021-04-07 07:47:56 -0300

    impr: creates a cart empty option with unit tests

--- src/components/CartDropdown/stories.tsx
+++ src/components/CartDropdown/stories.tsx
@@ -21,3 +21,9 @@ export const Default: Story<CartDropdownProps> = (args) => (
     <CartDropdown {...args} />
   </div>
 )
+
+export const Empty: Story<CartDropdownProps> = () => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown />
+  </div>
+)
