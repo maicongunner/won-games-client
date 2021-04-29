3b6167b58c4fb3a2be73ab89fd9b744f1bdb4b34
Maicon Domingues <maikonbdomingues@gmail.com>
2021-04-29 08:12:34 -0300

    impr: get the list games using useQuery from graphQL

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -6,39 +6,60 @@ import { Grid } from 'components/Grid'
 
 import * as S from './styles'
 import { KeyboardArrowDown } from '@styled-icons/material-outlined'
+import { useQuery } from '@apollo/client'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
-const handleFilter = () => {
-  return
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading, error } = useQuery<QueryGames, QueryGamesVariables>(
+    QUERY_GAMES,
+    { variables: { limit: 15 } }
+  )
+
+  const handleFilter = () => {
+    return
+  }
+
+  const handleShowMore = () => {
+    return
+  }
+
+  return (
+    <Base>
+      <S.Main>
+        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover!.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
+
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <KeyboardArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
+      </S.Main>
+    </Base>
+  )
 }
-
-const handleShowMore = () => {
-  return
-}
-
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => (
-  <Base>
-    <S.Main>
-      <ExploreSidebar items={filterItems} onFilter={handleFilter} />
-
-      <section>
-        <Grid>
-          {games.map((item) => (
-            <GameCard key={item.title} {...item} />
-          ))}
-        </Grid>
-
-        <S.ShowMore role="button" onClick={handleShowMore}>
-          <p>Show More</p>
-          <KeyboardArrowDown size={35} />
-        </S.ShowMore>
-      </section>
-    </S.Main>
-  </Base>
-)
 
 export default GamesTemplate
